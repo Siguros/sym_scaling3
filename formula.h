852534c48f6dce51532428bd5d3a36b254e3d75d
Siguros <47727992+Siguros@users.noreply.github.com>
2021-02-07 02:20:37 +0900

    Update formula.h

--- formula.h
+++ formula.h
@@ -52,5 +52,6 @@ double InvMeasuredLTP(double conductance, int maxNumLevel, std::vector<double>& 
 double InvMeasuredLTD(double conductance, int maxNumLevel, std::vector<double>& dataConductanceLTD);
 double getParamA(double NL);
 double NonlinearConductance(double C, double NL, double Vw, double Vr, double V);
-
+double getLinear(double A, double Pmax);
+double getSymmetric(double Altp, double Pmaxltp,double Altd, double Pmaxltd);
 #endif
