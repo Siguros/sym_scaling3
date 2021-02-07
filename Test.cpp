f77a8b208a01b97769f8901cc273558de1130237
Siguros <47727992+Siguros@users.noreply.github.com>
2021-02-07 12:15:55 +0900

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -141,24 +141,6 @@ void Validate() {
 							IsumMax += arrayIH->GetMaxCellReadCurrent(j,k);
 							IsumMin += arrayIH->GetMinCellReadCurrent(j,k);
 					
-						
-						for(int in=0; in<10; in++){
-						double s1= in/10;
-						double s2 =(in+1)/10;
-						int numcell= static_cast<AnalogNVM*>(arrayIH->cell[0][0])->NumCellperSynapse;
-						for(int jn=0;jn<numcell; jn++){
-						double s3 = static_cast<AnalogNVM*>(arrayIH->cell[0][0])->conductanceN[jn];
-						double minCon = static_cast<AnalogNVM*>(arrayIH->cell[0][0])->minConductance;
-						double maxCon = static_cast<AnalogNVM*>(arrayHO->cell[0][0])->maxConductance;
-						double s4 = (s3-minCon)/(maxCon-minCon);
-							if(s4>=s1){
-						if(s4<s2){
-						countpop[in]+=1;
-						}
-						} 
-						}
-
-						}
 						}
 						sumArrayReadEnergyIH += Isum * readVoltageIH * readPulseWidthIH;
 						int outputDigits = 2*(CurrentToDigits(Isum, IsumMax-IsumMin)-CurrentToDigits(inputSum, IsumMax-IsumMin));
@@ -402,9 +384,9 @@ void Validate() {
 					countNum = j;
 				}
 			}
-		}
-		if (testOutput[i][countNum] == 1) {
+				if (testOutput[i][countNum] == 1) {
 			correct++;
+		}
 		}
 	}
 	if (!param->useHardwareInTraining) {    // Calculate the classification latency and energy only for offline classification
@@ -415,8 +397,32 @@ void Validate() {
 		subArrayIH->readLatency += sumReadLatencyIH;
 		subArrayHO->readLatency += sumReadLatencyHO;
 	}
-			for(int in=0; in<10; in++){
+			
+		if (AnalogNVM *temp = dynamic_cast<AnalogNVM*>(arrayIH->cell[0][0])) {  // Analog eNVM
+						for (int j=0; j<param->nHide; j++){ 
+						for (int i=0; i<param->nInput; i++) {					
+						for(int in=0; in<10; in++){
+						double s1= in/10;
+						double s2 =(in+1)/10;
+						int numcell= static_cast<AnalogNVM*>(arrayIH->cell[0][0])->NumCellperSynapse;
+						for(int jn=0;jn<numcell; jn++){
+						double s3 = static_cast<AnalogNVM*>(arrayIH->cell[i][j])->conductanceN[jn];
+						double minCon = static_cast<AnalogNVM*>(arrayIH->cell[0][0])->minConductance;
+						double maxCon = static_cast<AnalogNVM*>(arrayHO->cell[0][0])->maxConductance;
+						double s4 = (s3-minCon)/(maxCon-minCon);
+						if(s4>=s1){
+						if(s4<s2){
+						countpop[in]+=1;
+						}
+						} 
+						}
+
+						}
+						}
+				for(int in=0; in<10; in++){
 				std::cout<<countpop[in]<<std::endl;
 			}
+						} 
+
 }
 
